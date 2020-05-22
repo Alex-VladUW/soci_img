9764cdd2da9f85b9a71100d2692891f2075f48f2
Alex-VladUW <65693647+Alex-VladUW@users.noreply.github.com>
2020-05-22 14:32:10 -0700

    Update 0x07_testSetBrightness.ino

--- tests/v1.1.0-probing/0x07_testSetBrightness.ino
+++ tests/v1.1.0-probing/0x07_testSetBrightness.ino
@@ -17,6 +17,104 @@
 
 // remember- ask me if you have questions! 
 // Use the arduino forums or stackOverflow. The internet is a great resource
- void testSetBrightness(){
-    Serial.println(F("Not implemented yet")); //Use Serial.print to help you debug!
- }
+void loop{
+   void testSetBrightness(){
+
+    Serial.println(F("SetBrightness")); //Use Serial.print 
+
+    for (byte value = 0x00; value <= 0x04; value++){//Brightness Value normal is 0x02
+
+    changeBrightness(value); //Set the brightness 
+
+    takePicture(value); //Takes a picture and sends it to the given slot(value)
+
+  } 
+
+}
+
+
+
+//Changes the Brightness Value
+
+//Checks is the system responded
+
+void changeBrightness(byte value) {
+
+  sendCommand(SET_BRIGHTNESS, value);
+
+  for(int i = 0; i < MAX_RESPONSE_BYTES; i++) {    
+
+    while(Serial.available() == 0) 
+
+    responseBytes[i] = Serial.read();
+
+    }
+
+    if(responseBytes[0] != ACK ||
+
+       responseBytes[1] != SET_BRIGHTNESS || 
+
+       responseBytes[2] != value) {
+
+      Serial.print("\nFAIL: Did not give proper response when Value (setBRIGHTNESS) = "); Serial.println(value);
+
+      Serial.print("Received: "); 
+
+      for(int j = 0; j < MAX_RESPONSE_BYTES; j++) {
+
+        Serial.print(responseBytes[j]); Serial.print(" ");
+
+      }
+
+      Serial.println("test");
+
+    }  
+
+}
+
+
+
+//Takes a picture and saves it in the selected slot
+
+void takePicture(byte value) {
+
+  sendCommand(TAKE_PICTURE,value);
+
+  for(int i = 0; i < MAX_RESPONSE_BYTES; i++) {    
+
+    if(Serial.available() == 0) {
+
+      Serial.println("No response, breaking (takePicture)");
+
+      break;      
+
+    }
+
+    responseBytes[i] = Serial.read();
+
+    }
+
+    if(responseBytes[0] != ACK ||
+
+       responseBytes[1] != TAKE_PICTURE || 
+
+       responseBytes[2] != value) {
+
+      Serial.print("\nFAIL: Did not give proper response when Value (Slot) (takePicture)  = "); Serial.println(value);
+
+      Serial.print("Received: "); 
+
+      for(int j = 0; j < MAX_RESPONSE_BYTES; j++) {
+
+        Serial.print(responseBytes[j]); Serial.print(" ");
+
+      }
+
+      Serial.println("test");
+
+    }
+
+  
+
+}
+}
